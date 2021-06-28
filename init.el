70e5003c13d2e8fd0907c3b8079c4c2e229dec01
Roman <Levkovych>
2021-06-29 02:17:51 +0300

    Removed some pkgs

--- init.el
+++ init.el
@@ -1,16 +1,16 @@
-;; My init.el file
+;;My init.el file
 ;; ver. 0.3
 
-;; setup package management
+;;setup package management
 (require 'package)
 (setq package-archives
       `(,@package-archives
         ("melpa" . "https://melpa.org/packages/")
-        ;; ("marmalade" . "https://marmalade-repo.org/packages/")
+;;        ("marmalade" . "https://marmalade-repo.org/packages/")
         ("org" . "https://orgmode.org/elpa/")
-        ;; ("user42" . "https://download.tuxfamily.org/user42/elpa/packages/")
-        ;; ("emacswiki" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/emacswiki/")
-        ;; ("sunrise" . "http://joseito.republika.pl/sunrise-commander/")
+;;        ("user42" . "https://download.tuxfamily.org/user42/elpa/packages/")
+        ("emacswiki" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/emacswiki/")
+;;        ("sunrise" . "http://joseito.republika.pl/sunrise-commander/")
         ))
 (package-initialize)
 
@@ -26,8 +26,8 @@
 
 (use-package use-package-core
   :custom
-  ;; (use-package-verbose t)
-  ;; (use-package-minimum-reported-time 0.005)
+  (use-package-verbose t)
+  (use-package-minimum-reported-time 0.005)
   (use-package-enable-imenu-support t))
 
 
@@ -42,46 +42,6 @@
   (setq quelpa-use-package-inhibit-loading-quelpa t)
   :ensure t)
 
-;; eshell
-(use-package em-smart
-  :defer t
-  :config
-  (eshell-smart-initialize)
-  :custom
-  (eshell-where-to-jump 'begin)
-  (eshell-review-quick-commands nil)
-  (eshell-smart-space-goes-to-end t))
-
-(use-package esh-help
-  :ensure t
-  :defer t
-  :config
-  (setup-esh-help-eldoc))
-
-(use-package esh-autosuggest
-  :ensure t
-  :hook (eshell-mode . esh-autosuggest-mode))
-
-
-(use-package eshell-prompt-extras
-  :ensure t
-  :after (eshell esh-opt)
-  :custom
-  (eshell-prompt-function #'epe-theme-dakrone))
-
-(use-package eshell-toggle
-  :ensure t
-  :after projectile
-  :custom
-  (eshell-toggle-use-projectile-root t)
-  (eshell-toggle-run-command nil)
-  :bind
-  ("M-`" . eshell-toggle))
-
-;; (use-package eshell-fringe-status
-;;   :ensure t
-;;   :hook
-;;   (eshell-mode . eshell-fringe-status-mode))
 
 ;; some highlighting
 (use-package paren
@@ -129,7 +89,7 @@
   (rainbow-identifiers-choose-face-function
    #'rainbow-identifiers-cie-l*a*b*-choose-face)
   :hook
-  (emacs-lisp-mode . rainbow-identifiers-mode) ; actually, turn it off
+  (emacs-lisp-mode . rainbow-identifiers-mode) actually, turn it off
   (prog-mode . rainbow-identifiers-mode))
 
 (use-package rainbow-mode
@@ -215,33 +175,126 @@
   (nameless-private-prefix t))
 
 ;; let's try evil mode
-(use-package evil
-  :ensure t)
+;; (use-package evil
+;;  :ensure t)
 
-;; (load-theme 'manoj-dark t)
+(load-theme 'manoj-dark t)
 
-(use-package lor-theme
-  :config
-  (load-theme 'lor t)
-  :quelpa
-  (lor-theme :repo "a13/lor-theme" :fetcher github :version original))
+(use-package display-line-numbers
+  :bind ("<f6> l" . display-line-numbers-mode))
 
-(use-package paradox
+;; Modernise package menu
+
+;; (use-package paradox
+;;   :ensure t
+;;   :defer 1
+;;   :config
+;;   (paradox-enable))
+
+
+;; Let's optimise a little bit
+
+(use-package gcmh
   :ensure t
-  :defer 1
-  :config
-  (paradox-enable))
+  :init
+  (gcmh-mode 1))
 
-;; Markup mode
-
-(use-package markdown-mode
-  :ensure t
-  :ensure-system-package markdown
-  :mode (("\\`README\\.md\\'" . gfm-mode)
-         ("\\.md\\'"          . markdown-mode)
-         ("\\.markdown\\'"    . markdown-mode))
+;; disable suspend on C-z
+(use-package frame
+  :bind
+  ("C-z" . nil)
   :custom
-  (markdown-command "markdown"))
+  (initial-frame-alist '((vertical-scroll-bars))))
+
+;; C-c C-g always quits minibuffer
+(use-package delsel
+  :bind
+  (:map mode-specific-map
+        ("C-g" . minibuffer-keyboard-quit)))
+
+;; dired
+
+;; make dired faster
+(use-package async
+  :ensure t
+  :defer t
+  :custom
+  (dired-async-mode 1))
+
+;; localization
+(use-package mule
+  :defer 0.1
+  :config
+  (prefer-coding-system 'utf-8)
+  (set-language-environment "UTF-8")
+  (set-terminal-coding-system 'utf-8))
+
+;; fancy gui
+
+(load-theme 'manoj-dark)
+
+(use-package olivetti
+  :ensure t
+  :custom
+  (olivetti-body-width 95))
+
+(use-package font-lock+
+  :defer t
+  :quelpa
+  (font-lock+ :repo "emacsmirror/font-lock-plus" :fetcher github))
+
+(use-package all-the-icons
+  :ensure t
+  :defer t
+  :config
+  (setq all-the-icons-mode-icon-alist
+        `(,@all-the-icons-mode-icon-alist
+          (package-menu-mode all-the-icons-octicon "package" :v-adjust 0.0)
+          (jabber-chat-mode all-the-icons-material "chat" :v-adjust 0.0)
+          (jabber-roster-mode all-the-icons-material "contacts" :v-adjust 0.0)
+          (telega-chat-mode all-the-icons-fileicon "telegram" :v-adjust 0.0
+                            :face all-the-icons-blue-alt)
+          (telega-root-mode all-the-icons-material "contacts" :v-adjust 0.0))))
+
+(use-package all-the-icons-dired
+  :ensure t
+  :hook
+  (dired-mode . all-the-icons-dired-mode))
+
+(use-package winner
+  :config
+  (winner-mode 1))
+
+(use-package mwheel
+  :custom
+  (mouse-wheel-scroll-amount '(1
+                               ((shift) . 5)
+                               ((control))))
+  (mouse-wheel-progressive-speed nil))
+
+(use-package pixel-scroll
+  :config
+  (pixel-scroll-mode))
+
+(use-package tooltip
+  :defer t
+  :custom
+  (tooltip-mode -1))
+
+;; (use-package time
+;;   :defer t
+;;   :custom
+;;   (display-time-default-load-average nil)
+;;   (display-time-24hr-format t)
+;;   (display-time-mode t))
+
+;; (use-package fancy-battery
+;;   :ensure t
+;;   :hook
+;;   (after-init . fancy-battery-mode))
+
+
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -249,10 +302,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(lor-theme paradox evil nameless ipretty suggest eros highlight-sexp highlight-quoted highlight-defined rainbow-mode rainbow-identifiers rainbow-delimiters quelpa-use-package page-break-lines hl-todo highlight-numbers highlight-escape-sequences eshell-toggle eshell-prompt-extras esh-help esh-autosuggest company-shell company-quickhelp)))
+   '(fancy-battery mood-line lor-theme paradox evil nameless ipretty suggest eros highlight-sexp highlight-quoted highlight-defined rainbow-mode rainbow-identifiers rainbow-delimiters quelpa-use-package page-break-lines hl-todo highlight-numbers highlight-escape-sequences eshell-toggle eshell-prompt-extras esh-help esh-autosuggest company-shell company-quickhelp)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(hl-todo ((t (:inherit hl-todo :italic t)))))
+ '(hl-todo ((t (:inherit hl-todo :italic t))))
+ '(mode-line ((t (:inherit default (:box (:line-width -1 :style released-button)))))))
